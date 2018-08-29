4d00e38067da0fee787e2c7ef51b5665443e31d1
Jeffrey Chea <jeffreychea234@gmail.com>
2018-08-29 10:29:32 -0500

    add standalone text generation file

--- gen.py
+++ gen.py
@@ -52,7 +52,6 @@ model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-model.load_weights('./models/text_gen.h5')
 
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
@@ -64,44 +63,33 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 
-def on_epoch_end(epoch, logs):
-    # Function invoked at end of each epoch. Prints generated text.
+model.load_weights('./models/text_gen.h5')
+
+print()
+print('----- Generating text: ')
+
+start_index = random.randint(0, len(text) - maxlen - 1)
+for diversity in [0.2, 0.5, 1.0, 1.2]:
+    print('----- diversity:', diversity)
+
+    generated = ''
+    sentence = text[start_index: start_index + maxlen]
+    generated += sentence
+    print('----- Generating with seed: "' + sentence + '"')
+    sys.stdout.write(generated)
+
+    for i in range(400):
+        x_pred = np.zeros((1, maxlen, len(chars)))
+        for t, char in enumerate(sentence):
+            x_pred[0, t, char_indices[char]] = 1.
+
+        preds = model.predict(x_pred, verbose=0)[0]
+        next_index = sample(preds, diversity)
+        next_char = indices_char[next_index]
+
+        generated += next_char
+        sentence = sentence[1:] + next_char
+
+        sys.stdout.write(next_char)
+        sys.stdout.flush()
     print()
-    print('----- Generating text after Epoch: %d' % epoch)
-
-    start_index = random.randint(0, len(text) - maxlen - 1)
-    for diversity in [0.2, 0.5, 1.0, 1.2]:
-        print('----- diversity:', diversity)
-
-        generated = ''
-        sentence = text[start_index: start_index + maxlen]
-        generated += sentence
-        print('----- Generating with seed: "' + sentence + '"')
-        sys.stdout.write(generated)
-
-        for i in range(400):
-            x_pred = np.zeros((1, maxlen, len(chars)))
-            for t, char in enumerate(sentence):
-                x_pred[0, t, char_indices[char]] = 1.
-
-            preds = model.predict(x_pred, verbose=0)[0]
-            next_index = sample(preds, diversity)
-            next_char = indices_char[next_index]
-
-            generated += next_char
-            sentence = sentence[1:] + next_char
-
-            sys.stdout.write(next_char)
-            sys.stdout.flush()
-        print()
-    model.save_weights('./models/text_gen.h5')
-
-print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
-
-
-model.fit(x, y,
-          batch_size=167,
-          epochs=60,
-          callbacks=[print_callback])
-
-model.save_weights('./models/text_gen.h5')
